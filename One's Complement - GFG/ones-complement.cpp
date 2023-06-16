c3b3f84e95afd63663b9d5b740cf64108a62bacd
vivek singh <viveksinghrajpoot274@gmail.com>
2023-06-16 14:11:40 +0530

    Added solution - LeetHub

---
+++ One's Complement - GFG/ones-complement.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{
+public:
+    int onesComplement(int N){
+        if(N == 0) return 1;
+        int ans=0,x=1;
+        while(N){
+            if(!(N & 1)) ans+=x;
+            x *=2;
+            N >>= 1;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        Solution ob;
+        cout<<ob.onesComplement(n)<<"\n";
+    }
+}
+// } Driver Code Ends
