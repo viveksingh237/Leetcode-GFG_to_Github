b527a3d9e2fed42665db2e130068dc66b0cc1943
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-10 18:50:20 +0530

    Time: 8 ms (98.67%), Space: 13.1 MB (85.83%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -21,59 +21,67 @@ public:
 
 
         // //*** Solution using hashing
-        // int n=matrix.size(),m=matrix[0].size();
-        // vector<int> row(n,0),col(m,0);
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         if(matrix[i][j] == 0){
-        //         row[i]= -1;
-        //         //cout<<i<<" ";
-        //         col[j]= -1;
-        //        // cout<<j<<endl;
-        //         }
-        //     }
-        // }
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> row(n,0);
+        vector<int> col(m,0);
+       for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                // mark ith index of row wih 1:
+                row[i] = 1;
 
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         if(row[i]== -1 || col[j] == -1){
-        //         matrix[i][j] == 0;
-        //         }
-        //     }
-        // }
+                // mark jth index of col wih 1:
+                col[j] = 1;
+            }
+        }
+    }
+
+    // Finally, mark all (i, j) as 0
+    // if row[i] or col[j] is marked with 1.
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (row[i] || col[j]) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
 
 
          //**** without using extra space Optimal
 
-        int col0=1;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                if(matrix[i][j] == 0){
-                    matrix[i][0] = 0;
-                    if(j != 0)
-                    matrix[0][j] =0;
-                    else
-                    col0 = 0;
-                }
-            }
-        }
+        // int col0=1;
+        // for(int i=0;i<matrix.size();i++){
+        //     for(int j=0;j<matrix[0].size();j++){
+        //         if(matrix[i][j] == 0){
+        //             matrix[i][0] = 0;
+        //             if(j != 0)
+        //             matrix[0][j] =0;
+        //             else
+        //             col0 = 0;
+        //         }
+        //     }
+        // }
 
-        for(int i=1;i<matrix.size();i++){
-            for(int j=1;j<matrix[0].size();j++){
-                if(matrix[i][j] != 0){
-                if(matrix[0][j] == 0 or matrix[i][0] == 0)
-                matrix[i][j] =0;
-            }
-            }
-        }
-        if(matrix[0][0] == 0){
-            for(int i=0;i<matrix[0].size();i++)
-            matrix[0][i]=0;
-        }
+        // for(int i=1;i<matrix.size();i++){
+        //     for(int j=1;j<matrix[0].size();j++){
+        //         if(matrix[i][j] != 0){
+        //         if(matrix[0][j] == 0 or matrix[i][0] == 0)
+        //         matrix[i][j] =0;
+        //     }
+        //     }
+        // }
+        // if(matrix[0][0] == 0){
+        //     for(int i=0;i<matrix[0].size();i++)
+        //     matrix[0][i]=0;
+        // }
 
-        if(col0 ==0){
-            for(int i=0;i<matrix.size();i++)
-            matrix[i][0] = 0;
-        }
+        // if(col0 ==0){
+        //     for(int i=0;i<matrix.size();i++)
+        //     matrix[i][0] = 0;
+        // }
+
+        
     }
 };
