5e865d2cffd6e83dbfafc4112d702b77b1a53330
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-10 14:13:24 +0530

    Time: 6 ms (31.88%), Space: 8.2 MB (15.6%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,53 @@
 class Solution {
 public:
-void solve(vector<vector<int>> &ans,vector<int> &ds,vector<int> &nums,vector<int> &freq){
-    if(ds.size() == nums.size()){
-       ans.push_back(ds);
-       return;
+void solve(int ind,vector<vector<int>> &ans,vector<int> &nums){
+    if(ind == nums.size()){
+        vector<int> temp;
+        for(int i=0;i<nums.size();i++)
+        temp.push_back(nums[i]);
+        ans.push_back(temp);
+        return;
     }
-    for(int i=0;i<nums.size();i++){
-        if(freq[i] == 0){
-            freq[i] =true;
-            ds.push_back(nums[i]);
-            solve(ans,ds,nums,freq);
-            ds.pop_back();
-            freq[i] = false;
+    for(int i=ind;i<nums.size();i++){
+            swap(nums[i],nums[ind]);
+            solve(ind+1,ans,nums);
+            swap(nums[i],nums[ind]);
         }
-    }
+    
 }
    
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds,freq(nums.size(),0);
-        solve(ans,ds,nums,freq);
+        solve(0,ans,nums);
         return ans;
 
     }
+
+
+
+
+    //************ using extra space
+//     void solve(vector<vector<int>> &ans,vector<int> &ds,vector<int> &nums,vector<int> &freq){
+//     if(ds.size() == nums.size()){
+//        ans.push_back(ds);
+//        return;
+//     }
+//     for(int i=0;i<nums.size();i++){
+//         if(freq[i] == 0){
+//             freq[i] =true;
+//             ds.push_back(nums[i]);
+//             solve(ans,ds,nums,freq);
+//             ds.pop_back();
+//             freq[i] = false;
+//         }
+//     }
+// }
+   
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> ds,freq(nums.size(),0);
+//         solve(ans,ds,nums,freq);
+//         return ans;
+
+//     }
 };
