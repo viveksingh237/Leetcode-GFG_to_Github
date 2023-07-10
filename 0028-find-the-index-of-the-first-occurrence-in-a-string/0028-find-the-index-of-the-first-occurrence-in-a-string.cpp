782fdddc18e73202ebb83d2d1d4b992055303182
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-10 16:01:56 +0530

    Time: 0 ms (100%), Space: 6.3 MB (56.68%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int ind=haystack.find(needle);
-        return (ind != -1) ? ind : -1;
+        // int ind=haystack.find(needle);
+        // return (ind != -1) ? ind : -1;
+        int m=haystack.size(),n=needle.size();
+        for(int i=0;i<=m-n;i++){
+            int j=0;
+            for(;j<n;j++){
+                if(haystack[i+j] != needle[j])
+                break;
+            }
+            if( j == n)
+            return i;
+        }
+        return -1;
+
     }
 };
