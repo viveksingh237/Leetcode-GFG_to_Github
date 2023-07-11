144960536c3dca6731597a165032e07e04da5ead
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-11 08:23:50 +0530

    Time: 3 ms (87.19%), Space: 11.1 MB (85.66%) - LeetHub

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/2015-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/2015-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     bool findRotation(vector<vector<int>>& matrix, vector<vector<int>>& target) {
-        // int x=0;
-        // int n=mat.size();
-        // while(x < 1){
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++)  // transpose
-        //     swap(mat[i][j],mat[j][i]);
-        // }
+        int x=0;
+        int n=matrix.size();
+        while(x < 4 and matrix != target){
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++)  // transpose
+            swap(matrix[i][j],matrix[j][i]);
+        }
         
-        //  for(int i=0;i<n;i++){
-        //     for(int j=0;j<n/2;j++)  // reversing the rows
-        //     swap(mat[i][j],mat[i][n-j-1]);
-        // }
-        // x++;
-        // }
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //     cout<<mat[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        // return mat == target;
+         for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+        x++;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+            cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        return matrix == target;
 
         //****** using extra space
-int x=0;
-int n = matrix.size();
-vector < vector < int >> rotated = matrix;
-while( x < 4 and rotated != target){
-        matrix = rotated;
-    for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-        rotated[j][n - i - 1] = matrix[i][j];
-    }
-    }
-    x++;
-}
-    return rotated == target;
+// int x=0;
+// int n = matrix.size();
+// vector < vector < int >> rotated = matrix;
+// while( x < 4 and rotated != target){
+//         matrix = rotated;
+//     for (int i = 0; i < n; i++) {
+//     for (int j = 0; j < n; j++) {
+//         rotated[j][n - i - 1] = matrix[i][j];
+//     }
+//     }
+//     x++;
+// }
+//     return rotated == target;
     }
 };
