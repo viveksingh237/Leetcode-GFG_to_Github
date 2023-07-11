aebfb29db50a6375e3cd30892865291d91a2deb9
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-11 07:44:48 +0530

    Time: 6 ms (22.83%), Space: 7.2 MB (13.87%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -17,7 +17,7 @@ public:
             for(int j=i;j<matrix.size();j++)
             swap(matrix[i][j],matrix[j][i]);
         }
-
+        // **** reversing the rows
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix.size()/2;j++)
             swap(matrix[i][j],matrix[i][matrix.size()-j-1]);
