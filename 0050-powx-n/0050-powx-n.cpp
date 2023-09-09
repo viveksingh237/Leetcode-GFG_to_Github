e80f8dfa9a3dacada9634ecebae927b358c0a717
vivek singh <viveksinghrajpoot274@gmail.com>
2023-09-09 11:03:09 +0530

    Time: 3 ms (39.84%), Space: 6 MB (29.43%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -3,11 +3,13 @@ public:
     double myPow(double x, int n) {
         double ans = 1.00;
         if(n < 0)
-        x = 1/x;
+        x = 1/x;   // in case power is negative
         n = abs(n);
+
             while(n){
              if(n & 1)
                 ans *= x ;
+                
                 x *= x;
                 n = n/2;
             }
